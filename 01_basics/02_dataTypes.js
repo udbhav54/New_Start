47ae56860802c1dc01e2808db0be2996afa6b797
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-10-29 14:46:48 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node.js not browser
+
+console.log(3 + 
+    23); // code readability should be high
+
+console.log(6 +6)
+
+let name = "udbhav"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string
+// booolean
+// null => stanalone value
+// undefined
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null) // Object
+
+console.log(typeof undefined) // undefined
