dcf6d534f904f3d7f621513d38c417d272303cf8
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-10-29 17:36:43 +0000

    Operation_conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score = "33abc" // NaN
+
+// let score = null // 0
+
+let score = undefined // NaN
+
+// console.log(typeof score);
+// console.log(typeof (score)); // function
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN => Not A Number
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
