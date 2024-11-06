a5aed53ecb6390a3048e12a7e1a7a95086cd72d6
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-11-06 08:43:30 +0000

    Updated

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,10 +8,15 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // value conversion problem
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0); // undefined != 0
 
 
+// === (the strict equality operator. It checks both value and type of the operands)
 
 
